1205888e1751189a5e779813ea21ece2e73f9072
Oriol Augé <oriolauge@gmail.com>
2022-03-05 13:23:21 +0100

    Add missing registration.php

---
+++ registration.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Module Redsys Registration
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'OAG_Redsys',
+    __DIR__
+);
